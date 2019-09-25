a046a12b5ef5114b2990f35ace7803a46988354d
Islam Sayed <islam.sayed8@gmail.com>
2019-09-25 14:24:19 +0200

    create comp

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ApplicationBar from './ApplicationBar';
+import { Hidden, Typography } from '@material-ui/core'
+const Header = () => {
+    return ( 
+        <header>
+            <ApplicationBar />
+                <img src={} alt="remote front-end jobs" />
+                <Typography variant="subtitle1">
+                    REMOTE FRONT-END JOBS
+                </Typography>
+            
+        </header>
+     );
+}
+ 
+export default Header;
