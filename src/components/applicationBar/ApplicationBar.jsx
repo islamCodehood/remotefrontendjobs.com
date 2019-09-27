e66c98e1f4bf093002133df77fa9f775bb9a39a4
Islam Sayed <islam.sayed8@gmail.com>
2019-09-27 13:56:36 +0200

    create comp

---
+++ src/components/applicationBar/ApplicationBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { AppBar, Typography, Button, IconButton, Toolbar  } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./ApplicationBar.css"
+
+const ApplicationBar = () => {
+
+  return (
+    <div className="root">
+      <AppBar position="static">
+        <Toolbar className="toolbar">
+          <Typography variant="h6" className="title">
+          R.F.E.J
+          </Typography>
+          <IconButton
+            edge="start"
+            className="menuButton" 
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default ApplicationBar;
