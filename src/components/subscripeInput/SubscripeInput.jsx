a35e81816c87ae730b0e074f3c58bc071fe7398a
Islam Sayed <islam.sayed8@gmail.com>
2019-09-27 13:55:57 +0200

    create comp

---
+++ src/components/subscripeInput/SubscripeInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubscripeInput = () => {
+    return ( 
+        <div>
+            <input type="email" />
+        </div>
+     );
+}
+ 
+export default SubscripeInput;
