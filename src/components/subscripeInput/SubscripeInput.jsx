aa85fb24fcb4e2dff06e1a2b38995c1410629721
Islam Sayed <islam.sayed8@gmail.com>
2019-09-28 02:45:57 +0200

    add text field comp

--- src/components/subscripeInput/SubscripeInput.jsx
+++ src/components/subscripeInput/SubscripeInput.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import "./SubscripeInput.css";
 const SubscripeInput = () => {
-    return ( 
-        <div>
-            <input type="email" />
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <TextField
+        id="outlined-email-input"
+        label="Subscripe"
+        className="email-input"
+        type="email"
+        name="email"
+        autoComplete="email"
+        margin="normal"
+        variant="filled"
+      />
+    </div>
+  );
+};
+
 export default SubscripeInput;
