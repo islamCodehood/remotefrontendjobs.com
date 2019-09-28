73bceedfc88599bb361fc54c8d3bdcb108e4a3bd
Islam Sayed <islam.sayed8@gmail.com>
2019-09-28 08:47:10 +0200

    use useStyle

--- src/components/subscripeInput/SubscripeInput.jsx
+++ src/components/subscripeInput/SubscripeInput.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import "./SubscripeInput.css";
+
+const useStyles = makeStyles(theme => ({
+    button: {
+      margin: "30px 0 0 8px",
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+
 const SubscripeInput = () => {
+  const classes = useStyles();
   return (
     <div>
       <TextField
@@ -12,8 +25,11 @@ const SubscripeInput = () => {
         name="email"
         autoComplete="email"
         margin="normal"
-        variant="filled"
+        /* variant="filled" */
       />
+      <Button variant="outlined" size="small" className={classes.button}>
+        Subscribe
+      </Button>
     </div>
   );
 };
