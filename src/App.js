cdd9d5b9bbc9419e976318897fe02e79a7ff0752
Islam Sayed <islam.sayed8@gmail.com>
2019-10-02 19:50:41 +0200

    beautify code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./pages/home/Home";
 function App() {
   const [jobs, setJobs] = useState([]);
   useEffect(() => {
-    collectJobs()
+    collectJobs();
   }, []);
 
   const collectJobs = () => {
@@ -15,57 +15,95 @@ function App() {
       "https://cors-anywhere.herokuapp.com/" +
         "https://stackoverflow.com/jobs/feed?q=front-end+developer&r=true"
     ).then(res => {
-      res.text().then(xmlTxt => {
-        const domParser = new DOMParser();
-        const doc = domParser.parseFromString(xmlTxt, "text/xml");
-        const items = doc.querySelectorAll("item")
-        const jobsArr = []
-        items.forEach((item, i) => {
-          const title = item.querySelector('title').textContent;
-          const author = item.querySelector('author').textContent;
-          const link = item.querySelector('link').textContent;
-          const categories = []
-          const categoriesNodes = item.querySelectorAll('category');
-          categoriesNodes.forEach(categoryNode => categories.push(categoryNode.textContent))
-          const description = item.querySelector('description').textContent;
-          const updateTime = item.querySelector('updated').textContent;
-          const id = sha256(title + author + link + updateTime)
-          const itemObj = {
-            title,
-            author,   
-            link,
-            categories,
-            description,
-            updateTime,
-            id,
-          }
-          jobsArr.push(itemObj)
-        })
-        
-        return jobsArr
-      }).then(jobsArr => {
+      res
+        .text()
+        .then(xmlTxt => {
+          const domParser = new DOMParser();
+          const doc = domParser.parseFromString(xmlTxt, "text/xml");
+          const items = doc.querySelectorAll("item");
+          const jobsArr = [];
+          items.forEach((item, i) => {
+            const title = item.querySelector("title").textContent;
+            const author = item.querySelector("author").textContent;
+            const link = item.querySelector("link").textContent;
+            const categories = [];
+            const categoriesNodes = item.querySelectorAll("category");
+            categoriesNodes.forEach(categoryNode =>
+              categories.push(categoryNode.textContent)
+            );
+            const description = item.querySelector("description").textContent;
+            const updateTime = item.querySelector("updated").textContent;
+            const id = sha256(title + author + link + updateTime);
+            const itemObj = {
+              title,
+              author,
+              link,
+              categories,
+              description,
+              updateTime,
+              id
+            };
+            jobsArr.push(itemObj);
+          });
 
-        filterJobs(jobsArr)
-      })
+          return jobsArr;
+        })
+        .then(jobsArr => {
+          filterJobs(jobsArr);
+          cutTitle(jobsArr)
+        });
     });
+    const cutTitle = jobsArr => {const indices = jobsArr.map(job => job.title.split(' ').findIndex(titleWord => titleWord === "at"))
+    console.log(indices)
+    
+  }
 
     const sortJobsByUpdateDate = filteredJobs => {
-      filteredJobs.sort((a,b) => (a.updateTime < b.updateTime) ? 1 : ((b.updateTime < a.updateTime) ? -1 : 0)); 
-      setJobs(filteredJobs)
-    } 
+      filteredJobs.sort((a, b) =>
+        a.updateTime < b.updateTime ? 1 : b.updateTime < a.updateTime ? -1 : 0
+      );
+      setJobs(filteredJobs);
+    };
 
     const filterJobs = jobsArr => {
-      const keyWords = ["front-end", "front", "frontend", "javascript", "html", "css", "angular", "angularjs", "react", "reactjs", "react.js", "native", "angular.js", "angular2", "ember", "svelte", "web"]
-      const filteredJobs =  keyWords.map(keyWord => 
-        jobsArr.filter(job => job.title.toLowerCase().split(" ").includes(keyWord))
-      ).flat()
-      sortJobsByUpdateDate(filteredJobs)
-    } 
+      const keyWords = [
+        "front-end",
+        "front",
+        "frontend",
+        "javascript",
+        "html",
+        "css",
+        "angular",
+        "angularjs",
+        "react",
+        "reactjs",
+        "react.js",
+        "native",
+        "angular.js",
+        "angular2",
+        "ember",
+        "svelte",
+        "web"
+      ];
+      const filteredJobs = keyWords
+        .map(keyWord =>
+          jobsArr.filter(job =>
+            job.title
+              .toLowerCase()
+              .split(" ")
+              .includes(keyWord)
+          )
+        )
+        .flat();
+      sortJobsByUpdateDate(filteredJobs);
+    };
   };
-  return <div className="App">
-    <Header />
-    <Home jobs={jobs} />
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home jobs={jobs} />
+    </div>
+  );
 }
 
 export default App;
