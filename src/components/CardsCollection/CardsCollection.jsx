8479d02a27222d3a1b82d9bee6a331fe32e55db0
Islam Sayed <islam.sayed8@gmail.com>
2019-10-04 01:28:40 +0200

    add link prop

--- src/components/CardsCollection/CardsCollection.jsx
+++ src/components/CardsCollection/CardsCollection.jsx
@@ -7,7 +7,7 @@ const CardsCollection = (props) => {
         <ul className="cards-collection">
             {props.jobs.map(job => (
                 <li key={job.id}>
-                    <JobCard title={job.title} company={job.author} techs={job.categories} date={job.updateTime} />
+                    <JobCard title={job.title} company={job.author} techs={job.categories} date={job.updateTime} link={job.link} />
                 </li>
             ))}
         </ul>
