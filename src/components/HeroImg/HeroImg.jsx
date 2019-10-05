af03322de09046d9a3a73de0e6e6b954e48f2d4c
Islam Sayed <islam.sayed8@gmail.com>
2019-10-05 12:05:24 +0200

    import Typography

--- src/components/HeroImg/HeroImg.jsx
+++ src/components/HeroImg/HeroImg.jsx
@@ -4,9 +4,12 @@ import img1sm from '../../img/hero/img1sm.jpg';
 import img1md from '../../img/hero/img1md.jpg';
 import img1lg from '../../img/hero/img1lg.jpg';
 import "./HeroImg.css"
+import { Typography } from '@material-ui/core';
 const HeroImg = () => {
     return ( 
         <div className="heroImg-wrapper">
+            <Typography variant="h1" className="title-header">REMOTE FRONT-END JOBS</Typography>
+
             {/* <picture>
                 <source srcSet={img1md} media="(min-width: 1200px)" />
                 <source srcSet={img1md} media="(min-width: 768px)" />
