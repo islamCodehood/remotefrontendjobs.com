851808fbbefdec128a456a900979eba13b9b3ea4
Islam Sayed <islam.sayed8@gmail.com>
2019-10-11 16:41:17 +0200

    add title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ApplicationBar from "../applicationBar/ApplicationBar";
-import { Hidden, Typography } from "@material-ui/core";
+import { Hidden, Typography, IconButton } from "@material-ui/core";
 import HeroImg from "../HeroImg/HeroImg";
+import MenuIcon from "@material-ui/icons/Menu";
 import "./Header.css"
 import SubscripeInput from "../subscripeInput/SubscripeInput";
 const Header = () => {
@@ -9,9 +10,6 @@ const Header = () => {
     <header>
       {/* <ApplicationBar /> */}
       <HeroImg />
-      <div className="title-header-wrapper">
-        <Typography variant="h1" className="title-header">REMOTE FRONT-END JOBS</Typography>
-      </div>
       {/* <SubscripeInput /> */}
     </header>
   );
