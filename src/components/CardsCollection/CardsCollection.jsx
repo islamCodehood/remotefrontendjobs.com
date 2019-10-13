6a0467818580ae3189d73573e594fed4acca17ae
Islam Sayed <islam.sayed8@gmail.com>
2019-10-14 00:29:33 +0200

    add id prop

--- src/components/CardsCollection/CardsCollection.jsx
+++ src/components/CardsCollection/CardsCollection.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import JobCard from '../jobCard/JobCard'
-import './CardsCollection.css'
+import React from "react";
+import JobCard from "../jobCard/JobCard";
+import "./CardsCollection.css";
 
-const CardsCollection = (props) => {
-    return ( 
-        <ul className="cards-collection">
-            {props.jobs.map(job => (
-                <li key={job.id}>
-                    <JobCard title={job.title} company={job.author} techs={job.categories} date={job.updateTime} link={job.link} />
-                </li>
-            ))}
-        </ul>
-     );
-}
- 
+const CardsCollection = props => {
+  const { jobs, /* jobReqs */ } = props;
+  return (
+    <ul className="cards-collection">
+      {jobs.map((job, i) =>  (
+        <li key={job.id}>
+          <JobCard
+            title={job.title}
+            company={job.author}
+            techs={job.categories}
+            date={job.updateTime}
+            link={job.link}
+            id={job.id}
+            //jobReqs={jobReqs}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 export default CardsCollection;
